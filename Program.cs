33c2c3552e5a5bc109a54b0adefad6289bf2bdf9
Pavlo Skachkov <pavlo.skachkov@valtech.com>
2023-08-17 16:32:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using ChatGptTest;
+
+class Program
+{
+    static async Task Main()
+    {
+        while (true)
+        {
+            Console.Write("Enter your question: ");
+            var userInput = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(userInput))
+            {
+                Console.WriteLine("You need to type something");
+                continue;
+            }
+
+            if (userInput.ToLower() == "quit" || userInput.ToLower() == "exit")
+            {
+                break;
+            }
+
+            var response = await OpenAI.SendMessageAsync(userInput);
+
+            Console.WriteLine("ChatGPT: " + response);
+        }
+    }
+}
